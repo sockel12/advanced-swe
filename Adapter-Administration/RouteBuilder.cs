39d10a02be1767db299d0e6dab000e1bd1091359
Benjamin Appel <benjamin.appel1991@icloud.com>
2024-04-16 10:44:25 +0200

    aa

---
+++ Adapter-Administration/RouteBuilder.cs
@@ -0,0 +1,13 @@
+using Application_Code.Interfaces;
+
+namespace Adapter_Administration;
+
+public enum Protocol
+{
+    GET, POST, PUT, DELETE
+}
+
+public static class RouteBuilder
+{
+    
+}
--- Adapter-Administration/WebServer.cs
+++ Adapter-Administration/WebServer.cs
@@ -1,8 +1,18 @@
 using Adapter_Repositories;
 using Adapter_Store_CSV;
 using Application_Code.Interfaces;
+using CsvHelper;
 
 namespace Adapter_Administration;
+
+internal enum Method
+{
+    GET,
+    POST,
+    PUT,
+    UPDATE,
+    DELETE
+}
 
 public class WebServer
 {
@@ -31,16 +41,68 @@ public class WebServer
 
         app.UseHttpsRedirection();
 
-        if (!Routes.CreateRoutes(app))
-        {
-            Console.WriteLine("Failed to create routes.");
-        }
-        
         if (app.Environment.IsDevelopment())
         {
             MockDataGenerator.CreateMockData();
         }
 
+    }
+    
+    public void Get(string path, Action action)
+    {
+        SetupRoute(path, action, Method.GET);
+    }
+    
+    public void Post(string path, Action<object> action)
+    {
+        SetupRoute(path, action, Method.POST);
+
+    }
+    
+    public void Put(string path, Action action)
+    {
+        SetupRoute(path, action, Method.PUT);
+    }
+    
+    public void Update(string path, Action<object> action)
+    {
+        SetupRoute(path, action, Method.UPDATE);
+
+    }
+    
+    public void Delete(string path, Action<object> action)
+    {
+        SetupRoute(path, action, Method.DELETE);
+
+    }
+
+    private void SetupRoute(string path, Delegate callback, Method method)
+    {
+        Func<string, Delegate, RouteHandlerBuilder> mapFunction;
+        switch (method)
+        {
+            case Method.GET:
+                mapFunction = app.MapGet;
+                break;
+            case Method.POST:
+                mapFunction = app.MapPost;
+                break;
+            case Method.PUT:
+                mapFunction = app.MapPut;
+                break;
+            case Method.UPDATE:
+                mapFunction = app.MapPut;
+                break;
+            case Method.DELETE:
+                mapFunction = app.MapDelete;
+                break;
+            default:
+                return;
+        }
+        
+        mapFunction(path, callback)
+            .WithName($"{method.ToString()} {path}")
+            .WithOpenApi();
     }
 
     public void Start()
